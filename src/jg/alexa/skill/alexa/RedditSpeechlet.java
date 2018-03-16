5f362a76e6eaf49d9b39da73271c6ee215acdc2d
jgreelyBR <jason.greely@bloomreach.com>
2018-03-16 10:39:10 -0400

    project structure with sample speech assets

---
+++ src/jg/alexa/skill/alexa/RedditSpeechlet.java
@@ -0,0 +1,7 @@
+package jg.alexa.skill.alexa;
+
+/**
+ * Created by jgreely on 3/16/18.
+ */
+public class RedditSpeechlet {
+}
---
+++ src/jg/alexa/skill/reddit/RedditService.java
@@ -0,0 +1,7 @@
+package jg.alexa.skill.reddit;
+
+/**
+ * Created by jgreely on 3/16/18.
+ */
+public class RedditService {
+}
